cc49e805f24130c2e120a9f06835481674f2d84e
Saloni Tanmor <saloni.tanmor1@gmail.com>
2026-06-10 02:04:21 +0530

    java backend update

--- backend/src/main/java/com/nexusguard/backend/config/WebConfig.java
+++ backend/src/main/java/com/nexusguard/backend/config/WebConfig.java
@@ -10,18 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins(
-                    "http://localhost:3000",
-                    "http://localhost:5173",
-                    "http://13.48.249.157:3000",
-                    "http://13.61.154.100",
-                    "http://56.228.10.113:8001",
-                    "http://34.230.243.158:3000",
-                    "https://nexus-guard-h58t.vercel.app",
-                    "http://13.61.143.100:8000",
-                    "http://visual-analytics:8000",
-                    "http://16.170.208.158:8000"
-                )
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .maxAge(3600);
